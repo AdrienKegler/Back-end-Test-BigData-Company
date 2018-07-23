245bd8135908d86882833a5aa9aa4dce11ce5d16
AdrienKegler <adrien.kegler@viacesi.fr>
2018-07-23 23:17:24 +0200

    Fetching of links from URL -> OK

---
+++ Page.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Back_end_Test_BigData_Company
+{
+    public class Page
+    {
+        // Properties.
+        public string url { get; set; }
+        public uint depth { get; set; }
+        private String htmlCode { get; set; }
+        public List<Page> childrenList { get; set; }
+
+
+        // Constructor.
+        public Page(String url, uint depth = 0)
+        {
+            childrenList = new List<Page>();
+            this.url = url;
+            this.depth = depth;
+            // this.htmlCode = "<!DOCTYPE html><html><head><title>This is the title</title></head><body><a href=\"link 1\"></a><a href=\"link 2\"></a>	<a href=\"link 3\"></a><a href=\"link 4\"></a><a href=\"link 5\"></a><a href=\"link 6\"></a><p>Text 1</p><p>Text 2</p><p>Text 3</p><p>Text 4</p><p>Text 5</p><p>Text 6</p><p>Text 7</p></body></html>";
+            this.htmlCode = this.getHTMLCode();
+        }
+
+
+
+        // Methods
+
+        private String getHTMLCode()
+        {
+            String urlAddress = this.url;
+            String data = "";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (response.CharacterSet == null)
+                    {
+                        // default encoding is UTF8
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else
+                    {
+                        String charset = "";
+                        switch (response.CharacterSet)
+                        {
+                            case "\"utf-8\"": // this was the value of response.CharacterSet as tested on a specific website. Here is a dirty fix.
+                                charset = "utf-8";
+                                break;
+                            default:
+                                charset = "utf-8";
+                                break;
+                        }
+
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(charset));
+                    }
+
+                    data = readStream.ReadToEnd();
+
+                    response.Close();
+                    readStream.Close();
+                }
+            }
+            catch { }
+            return data;
+        }
+
+
+        public void dig(uint maxDepth)
+        {
+            if (this.depth < maxDepth)
+            {
+                this.setChildrenList();
+                if (this.depth < maxDepth - 1)
+                {
+                    foreach (Page children in this.childrenList)
+                    {
+                        children.dig(maxDepth);
+                    }
+                }
+            }
+        }
+
+
+        private void setChildrenList()
+        {
+            MatchCollection matches = Regex.Matches(this.htmlCode, "(<a.*?>.*?</a>)");
+
+            Console.WriteLine("Here is the list of the new pages found from " + this.url);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match m in matches)
+                {
+                    String tag = m.Groups[0].Value;
+                    // Matches the simple/double quote just after href then matches the following chars until meeting the string delimiter (simple/double quote LIKE the first match)
+                    String link = Regex.Match(tag, @"<a.*?href=([""'])(.*?)\1").Groups[2].Value;
+                    if (!link.StartsWith("http")) // handeling relative paths
+                    {
+                        String domainName = this.url.EndsWith("/") ? this.url.Remove(this.url.Length - 1) : this.url;
+                        domainName += "/";
+                        switch (link.ToCharArray()[0])
+                        {
+                            case '/':
+                                link.Remove(0, 1);
+                                break;
+                            case '#':
+                                domainName.Remove(0, 1);
+                                break;
+                            default:
+                                break;
+                        }
+                        link = domainName + link;
+                    }
+                    Page newChildren = new Page(link, this.depth + 1);
+                    this.childrenList.Add(newChildren);
+                    Console.WriteLine(link);
+
+                }
+            }
+            // Some space between each Pages so it's prettier
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Back_end_Test_BigData_Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, input your url :");
+            String url = Console.ReadLine();
+
+            Console.WriteLine("Please, input the wished recursing depth :");
+            uint depth = (uint) Math.Abs(Convert.ToInt16(Console.ReadLine()));
+            Page rootPage = new Page(url);
+
+            rootPage.dig(depth);
+        }
+    }
+}
